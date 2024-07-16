76e7924d356e2f075e4b67a7607c8aa71a66ffb9
bhuvanesh1440 <bhuvanesh1440@gmail.com>
2024-07-16 13:47:00 +0530

    frontend:

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Contact from './Contact.js';
 function App() {
   return (
     <>
-    <BrowserRouter>
+    <BrowserRouter basename='/Test_githubpages'>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
